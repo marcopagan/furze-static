a898b69ec5e0f3a75081d0cd469a4ff48b6b478e
marcopagan <marco.pagan.one@gmail.com>
2026-03-11 19:09:32 +0100

    Adding og:image, fix svg logo

--- app/immutable/entry/start.BdFXPOvO.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/sNOQJ4fZ.js";import{x as t}from"../chunks/jlUqCSnS.js";export{t as load_css,r as start};
---
+++ app/immutable/entry/start.CNalyhjA.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BRQzgzuc.js";import{x as t}from"../chunks/bqY99oT0.js";export{t as load_css,r as start};
