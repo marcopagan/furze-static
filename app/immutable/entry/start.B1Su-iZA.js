181ef1d9503d6937629d7a5365ab2977618b5037
marcopagan <marco.pagan.one@gmail.com>
2026-04-10 15:22:15 +0200

    Fixing logo size, about position, image order

--- app/immutable/entry/start.B1Su-iZA.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/zVaCm0gS.js";import{x as t}from"../chunks/BIXPtXfU.js";export{t as load_css,r as start};
---
+++ app/immutable/entry/start.g9135zdm.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/Cg1WvFvV.js";import{x as t}from"../chunks/uKhN_EzE.js";export{t as load_css,r as start};
